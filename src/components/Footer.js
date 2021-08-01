4f0b2f051a2beb437edcd62bb68cd5b7be289310
Odeh <abuzaid163@hotmail.com>
2021-08-01 18:13:49 +0300

    components

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <>
+        <footer>
+          <h1 style={{ width: '100%', textAlign: 'center', fontSize: '22px' }}>
+            &copy; Odeh Abuzaid
+          </h1>
+        </footer>
+      </>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h1 style={{ width: '100%', textAlign: 'center', fontSize: '42px' }}>
+            Horned Beasts
+          </h1>
+        </header>
+      </>
+    );
+  }
+}
+
+export default Header;
