ca47de457d19837c62ccef0b10b7f206c88a6f02
Odeh <abuzaid163@hotmail.com>
2021-08-03 15:30:29 +0300

    Feature1+theStretchGoal

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import SelectedBeast from './SelectedBeast';
 
 class HornedBeast extends React.Component {
   constructor(props) {
@@ -13,25 +15,44 @@ class HornedBeast extends React.Component {
       numberOfVotes: this.state.numberOfVotes + 1,
     });
   };
+  openModal = () => {
+    this.setState({ showModal: true });
+  };
+  handleClose = () => {
+    this.setState({ showModal: false });
+  };
+
   render() {
     const { image_url, title, description, keyword } = this.props;
     return (
-      <Card className='shadow rounded' style={{ minWidth: '20rem' }}>
-        <Card.Img
-          onClick={this.increaseNoOfVotes}
-          variant='top'
-          src={image_url}
+      <>
+        <Card className='shadow rounded' style={{ minWidth: '20rem' }}>
+          <Card.Img
+
+            onClick={this.openModal}
+            variant='top'
+            src={image_url}
+          />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>{description}</Card.Text>
+            <Card.Text>{keyword}</Card.Text>
+            <Card.Text></Card.Text>
+          </Card.Body>
+          <Card.Footer className='text-center'>
+            <Button variant="outline-info" onClick={this.increaseNoOfVotes}>
+                ❤️ {this.state.numberOfVotes}
+            </Button>
+          </Card.Footer>
+        </Card>
+        <SelectedBeast
+          description={this.props.description}
+          title={this.props.title}
+          imgSrc={this.props.image_url}
+          handleClose={this.handleClose}
+          show={this.state.showModal}
         />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>{description}</Card.Text>
-          <Card.Text>{keyword}</Card.Text>
-          <Card.Text></Card.Text>
-        </Card.Body>
-        <Card.Footer className='text-center'>
-          <small> ❤️ {this.state.numberOfVotes}</small>
-        </Card.Footer>
-      </Card>
+      </>
     );
   }
 }
